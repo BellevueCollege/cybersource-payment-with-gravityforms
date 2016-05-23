ac097372cce3f0d98be8665e7bed09c49b24ca6e
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2016-05-23 11:18:04 -0700

    Navigates user to cybersource payment page.
    1. Creates a hidden form with fields needed to make connection/redirect to cybersource payment.
    2. Concatenates the form to the original confirmation message created by web manager.

---
+++ cybersource-options.php
@@ -0,0 +1,85 @@
+<?php
+add_action('admin_init', 'cybersourceoptions_init' );
+add_action('admin_menu', 'cybersourceoptions_add_page');
+
+// Init plugin options to white list our options
+function cybersourceoptions_init(){
+	register_setting( 'cybersourceoptions_options', 'cybersource', 'cybersourceoptions_validate' );
+}
+
+// Add menu page
+function cybersourceoptions_add_page() {
+	add_options_page('CyberSource Options', 'CyberSource', 'manage_options', 'cybersourceoptions', 'cybersourceoptions_do_page');
+}
+
+// Draw the menu page itself
+function cybersourceoptions_do_page() {
+	?>
+	<div class="wrap">
+		<h2>Cyber Source Options</h2>
+		<form method="post" action="options.php">
+			<?php settings_fields('cybersourceoptions_options'); ?>
+			<?php $options = get_option('cybersource'); ?>
+			<table class="form-table">
+				<tr valign="top"><th scope="row">Cyber Source Access Key</th>
+					<td><input name="cybersource[cybersource_access_key]" type="text" value="<?php echo $options['cybersource_access_key']; ?>" /></td>
+				</tr>
+				<tr valign="top"><th scope="row">Cyber Source Form Post URL</th>
+					<td><input type="text" name="cybersource[cybersource_form_post_url]" value="<?php echo $options['cybersource_form_post_url']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Cyber Source Locale</th>
+					<td><input type="text" name="cybersource[cybersource_locale]" value="<?php echo $options['cybersource_locale']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Cyber Source Profile ID</th>
+					<td><input type="text" name="cybersource[cybersource_profile_id]" value="<?php echo $options['cybersource_profile_id']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Cyber Source Secret Key</th>
+					<td><input type="text" name="cybersource[cybersource_secret_key]" value="<?php echo $options['cybersource_secret_key']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Bill to Address Country</th>
+					<td><input type="text" name="cybersource[cybersource_bill_to_address_country]" value="<?php echo $options['cybersource_bill_to_address_country']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Bill to Address State </th>
+					<td><input type="text" name="cybersource[cybersource_bill_to_address_state]" value="<?php echo $options['cybersource_bill_to_address_state']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Currency</th>
+					<td><input type="text" name="cybersource[cybersource_currency]" value="<?php echo $options['cybersource_currency']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Transaction Type</th>
+					<td><input type="text" name="cybersource[cybersource_transaction_type]" value="<?php echo $options['cybersource_transaction_type']; ?>" /></td>
+				</tr>
+                                
+                                <tr valign="top"><th scope="row">Cyber Source Merchant ID</th>
+					<td><input type="text" name="cybersource[cybersource_merchant_id]" value="<?php echo $options['cybersource_merchant_id']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Cyber Source Report User</th>
+					<td><input type="text" name="cybersource[cybersource_report_user]" value="<?php echo $options['cybersource_report_user']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Cyber Source Report Password</th>
+					<td><input type="text" name="cybersource[cybersource_report_password]" value="<?php echo $options['cybersource_report_password']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Cyber Source Report URL</th>
+					<td><input type="text" name="cybersource[cybersource_report_url]" value="<?php echo $options['cybersource_report_url']; ?>" /></td>
+				</tr>
+                                <tr valign="top"><th scope="row">Cyber Source Shared Secret</th>
+					<td><input type="text" name="cybersource[cybersource_shared_secret]" value="<?php echo $options['cybersource_shared_secret']; ?>" /></td>
+				</tr>
+			</table>
+			<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			</p>
+		</form>
+	</div>
+	<?php	
+}
+
+// Sanitize and validate input. Accepts an array, return a sanitized array.
+function cybersourceoptions_validate($input) {
+	// Our first value is either 0 or 1
+	//$input['access_key'] = $input['access_key'];
+	
+	// Say our second option must be safe text with no HTML tags
+	//$input['form_post_url'] =  wp_filter_nohtml_kses($input['form_post_url']);
+	
+	return $input;
+}
---
+++ cybersource-payment-with-gravityforms.php
@@ -0,0 +1,119 @@
+<?php
+/*
+Plugin Name: CyberSource Payment With GravityForms
+Plugin URI: https://github.com/TriptiSharma/cybersource-payment-with-gravityforms
+Description: 
+Author: Bellevue College Technology Development and Communications
+Version: 1.0
+
+*/
+
+require_once("cybersource-options.php");
+// Adding Application Fees setting to Form Settings
+add_filter( 'gform_form_settings', 'form_fees_setting', 10, 2 );
+function form_fees_setting( $settings, $form ) {
+    $settings['Form Basics']['form_application_fees'] = '
+        <tr>
+            <th><label for="form_application_fees">Application Fees</label></th>
+            <td><input value="' . rgar($form, 'form_application_fees') . '" name="form_application_fees"></td>
+        </tr>';
+
+    return $settings;
+}
+
+// save your custom form setting
+add_filter( 'gform_pre_form_settings_save', 'save_my_custom_form_setting' );
+function save_my_custom_form_setting($form) {
+    $form['form_application_fees'] = rgpost( 'form_application_fees' );
+    return $form;
+}
+// End of Form Settings
+
+// Redirecting to cybersource using post data
+
+add_action( 'gform_confirmation', 'post_to_cybersource', 10, 4 );
+function post_to_cybersource($confirmation, $entry, $form ) {
+    $form_object = GFAPI::get_form($form['form_id']);    
+    $options = get_option('cybersource');
+    $secret_key = $options["cybersource_secret_key"];  
+    $post_url = $options['cybersource_form_post_url'];  
+    $reference_number = $form['form_id']."-".$entry['id'];  
+    $transaction_uuid = uniqid();   
+    $fields_map = array (
+                        'access_key' => $options['cybersource_access_key'],
+                        'locale' => $options['cybersource_locale'],                               
+                        'profile_id' => $options['cybersource_profile_id'],
+                        'bill_to_address_country' => $options['cybersource_bill_to_address_country'],
+                        'bill_to_address_state' => $options['cybersource_bill_to_address_state'],
+                        'currency' => $options['cybersource_currency'],
+                        'customer_ip_address' => $_SERVER['REMOTE_ADDR'],
+                        'item_0_name' => $form_object['title'],
+                        'item_0_quantity' => '1',
+                        'item_0_unit_price' => $form_object['form_application_fees'],
+                        'line_item_count' => '1',                        
+                        'reference_number' => $reference_number,
+                        'signed_date_time' => &$signed_date_time,
+                        'signed_field_names' => &$signed_field_names,
+                        'transaction_type' => $options['cybersource_transaction_type'],
+                        'transaction_uuid' => $transaction_uuid,
+                        'unsigned_field_names' => '',
+                      );
+   $signed_date_time = gmdate( 'Y-m-d\TH:i:s\Z' );
+   $signed_field_names = implode(',',array_keys( $fields_map ));
+   $signature = set_signature($fields_map,$secret_key) ;   
+   extract($fields_map);   
+
+//$confirmation =  "";
+$confirmation .= "<form name='redirect' action = '". $post_url . "'  method='POST'>";
+$confirmation .= "<input type='hidden' id= 'access_key' name='access_key' value='". $access_key . "'>";
+$confirmation .= "<input type='hidden' id='locale' name='locale' value='" .$locale . "'>";
+$confirmation .= "<input type='hidden' id='profile_id' name='profile_id' value='" . $profile_id . "'>";
+$confirmation .= "<input type='hidden' id='bill_to_address_country' name='bill_to_address_country' value='". $bill_to_address_country ."' />";
+$confirmation .= "<input type='hidden' id='bill_to_address_state' name='bill_to_address_state' value='". $bill_to_address_state ."' />";
+$confirmation .= "<input type='hidden' id='currency' name='currency' value='". $currency ."' />";
+$confirmation .= "<input type='hidden' id='customer_ip_address' name='customer_ip_address' value= '". $customer_ip_address ."' /> ";
+$confirmation .= "<input type='hidden' id='item_0_name' name='item_0_name' value='" . $item_0_name ."' />";
+$confirmation .= "<input type='hidden' id='item_0_quantity' name='item_0_quantity' value='". $item_0_quantity . "' />";
+$confirmation .= "<input type='hidden' id='item_0_unit_price' name='item_0_unit_price' value='" . $item_0_unit_price . "'>";
+$confirmation .= "<input type='hidden' id='line_item_count' name='line_item_count' value='" . $line_item_count . "' />";
+$confirmation .= "<input type='hidden' id='reference_number' name='reference_number' value= '". $reference_number ."' />";
+$confirmation .= "<input type='hidden' id='signed_date_time' name='signed_date_time' value='". $signed_date_time ."' /> ";
+$confirmation .= "<input type='hidden' id='signed_field_names' name='signed_field_names' value= '" . $signed_field_names ."' />";
+$confirmation .= "<input type='hidden' id='transaction_type' name='transaction_type' value='". $transaction_type ."' />";
+$confirmation .= "<input type='hidden' id='transaction_uuid' name='transaction_uuid' value='" . $transaction_uuid ."' /> ";
+$confirmation .= "<input type='hidden' id='unsigned_field_names' name='unsigned_field_names' value='". $unsigned_field_names ."' />";
+$confirmation .= "<input type='hidden' id='signature' name='signature' value='". $signature ."' />";
+//$confirmation .= "<p> Thank You. Please continue to payment. </p>";
+$confirmation .= "<input type='submit' value='Payment'>";
+$confirmation .= "</form>";
+
+
+return $confirmation;
+}
+
+function set_signature($fields_map,$secret_key) 
+{
+    $data_to_sign = '';
+    foreach ( $fields_map as $field => $value )
+    {
+            $data_to_sign .= "$field=$value,";
+    }
+    error_log("data : ".$data_to_sign);
+    // Remove trailing comma
+    $data_to_sign = substr( $data_to_sign, 0, -1 );
+    $hash = hash_hmac(
+            'sha256',
+            $data_to_sign,
+            $secret_key,
+            true
+    );
+    $signature = base64_encode( $hash );
+    return $signature;
+}
+
+?>
+
+
+
+
+
